9f39fbb1e0d23c923cf81a0342d7fd13fe8d9cfd
Andrej Babic <andrej.babic@psi.ch>
2018-04-19 16:02:43 +0200

    New library version
    - Writer handles changes in data sources.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="sf_bsread_writer",
-      version="0.0.2",
+      version="1.0.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
