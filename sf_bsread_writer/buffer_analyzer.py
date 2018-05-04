8875d608b99149485a0c39719c242144db8c7d94
Andrej Babic <andrej.babic@psi.ch>
2018-05-04 10:04:42 +0200

    Basic analyzer capabilities

--- sf_bsread_writer/buffer_analyzer.py
+++ sf_bsread_writer/buffer_analyzer.py
@@ -1,2 +1,11 @@
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
 def analyze_message(message):
-    pass
+    pulse_id = message.data.pulse_id
+
+    for name, value in message.data.data.items():
+        if value.value is None:
+            _logger.debug("Channel '%s' data missing in pulse_id %d.", name, pulse_id)
