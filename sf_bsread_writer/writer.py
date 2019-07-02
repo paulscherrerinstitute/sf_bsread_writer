87bf051d8a9149c2c4a99af7dc4319a45c12efc0
Andrej Babic <andrej.babic@psi.ch>
2019-07-02 16:03:00 +0200

    Refactor REST Api into separate module

--- sf_bsread_writer/writer.py
+++ sf_bsread_writer/writer.py
@@ -5,10 +5,11 @@ from threading import Event, Thread
 import bottle
 import os
 
-from bsread import PULL, source, json
+from bsread import PULL, source
 from bsread.handlers import extended
 
 from sf_bsread_writer.writer_format import BsreadH5Writer
+from sf_bsread_writer.writer_rest import register_rest_interface
 
 _logger = logging.getLogger(__name__)
 
@@ -152,62 +153,6 @@ class BsreadWriterManager(object):
         return {"start_pulse_id": self.start_pulse_id,
                 "stop_pulse_id": self.stop_pulse_id,
                 "last_pulse_id": self.last_pulse_id}
-
-
-def register_rest_interface(app, manager):
-    @app.get("/status")
-    def get_status():
-        return {"state": "ok",
-                "status": manager.get_status()}
-
-    @app.post("/parameters")
-    def set_parameters():
-        manager.set_parameters(bottle.request.json)
-
-        return {"state": "ok",
-                "status": manager.get_status(),
-                "parameters": manager.get_parameters()}
-
-    @app.get("/stop")
-    def stop():
-        manager.stop()
-
-        return {"state": "ok",
-                "status": manager.get_status()}
-
-    @app.get("/kill")
-    def kill():
-        os._exit(0)
-
-    @app.get("/statistics")
-    def get_statistics():
-        return {"state": "ok",
-                "status": manager.get_status(),
-                "statistics": manager.get_statistics()}
-
-    @app.put("/start_pulse_id/<pulse_id>")
-    def start_pulse_id(pulse_id):
-        _logger.info("Received start_pulse_id %s.", pulse_id)
-
-        manager.start_writer(int(pulse_id))
-
-    @app.put("/stop_pulse_id/<pulse_id>")
-    def stop_pulse_id(pulse_id):
-        _logger.info("Received stop_pulse_id %s.", pulse_id)
-
-        manager.stop_writer(int(pulse_id))
-
-    @app.error(500)
-    def error_handler_500(error):
-        bottle.response.content_type = 'application/json'
-        bottle.response.status = 200
-
-        error_text = str(error.exception)
-
-        _logger.error(error_text)
-
-        return json.dumps({"state": "error",
-                           "status": error_text})
 
 
 def start_server(stream_address, output_file, user_id, rest_port):
